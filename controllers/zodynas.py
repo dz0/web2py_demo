203ad0a20a31d5370d91f20a415a574319962c23
dz0 <jurgis.pralgauskis@gmail.com>
2017-02-08 15:20:56 +0200

    žodynų intro

---
+++ controllers/zodynas.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""
+  Duomenų saugojimo lentele ypatumai..
+
+  sarasas iš skaičių:
+  kiek
+  -------
+ [ 10 ,
+  -20 ,
+   55 , ]
+  
+  sarasas iš sąrašų:
+  kiek  uz_ka
+    0      1
+  ------------
+ [
+  [10,     "premija"], 
+  [-20,    "pietūs"],
+  [55,     "pasisekė loterijoj"],
+ ]
+  for kiek, uz_ka in sarasas:
+      if kiek  > 0:
+          print "gavau", kiek, 'uz', uz_ka  #  gražus pythoniškas
+  
+ 
+ 
+  sąrašas iš ŽODYNŲ:
+  ---------------------
+ [
+     { 'kiek': 10,  'uz_ka': "premija"  },
+     { 'kiek':-20,  'uz_ka': "pietūs"   },
+ ]
+  
+  for row in sarasas:
+      row = Storage( row )  # web2py įprastas duomenų laikymas  
+      
+      if row.kiek  > 0:
+          print "gavau", row.kiek, 'uz', row.uz_ka  
+          
+          # python su žodynas
+          # print "gavau", row['kiek'], 'uz', row['uz_ka']  
+                    
+"""
